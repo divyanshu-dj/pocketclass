1e223a3ee1c792f24530e261d9b8c1b54ff7c130
Andrew Liu <aliu5454@gmail.com>
2022-10-20 13:26:57 -0400

    banner button, search fix, and info card update

--- components/Header.js
+++ components/Header.js
@@ -61,7 +61,7 @@ function Header({ placeholder }) {
             {/* middle - search*/}
             <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
                 <input value={searchInput} id="searchInputId"
-                    onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder={placeholder || "tennis, painting, violin..."} className='text-gray-600 placeholder-gray-400 text-sm flex-grow pl-5 bg-transparent border-none focus:outline-none' />
+                    onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder={placeholder || "tennis, painting, violin..."} className='text-gray-600 placeholder-gray-400 text-sm flex-grow pl-5 bg-transparent border-transparent focus:border-transparent focus:ring-0' />
                 <SearchIcon onClick={search} className='md:mx-2 hidden md:inline-flex h-8 bg-logo-red text-white rounded-full p-2 cursor-pointer hover:scale-105 transition transform duration-200 ease-out active:scale-90 transition duration-150' />
             </div>
 
