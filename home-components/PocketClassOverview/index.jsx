128689aba1b4b52b3e5350dec2ff01b206c8153d
Andrew Liu <aliu5454@gmail.com>
2024-11-21 19:12:55 -0800

    Update index.jsx

--- home-components/PocketClassOverview/index.jsx
+++ home-components/PocketClassOverview/index.jsx
@@ -1,7 +1,7 @@
 function PocketClassOverview() {
   return (
     <div className="flex justify-start items-stretch flex-col w-[100.00%] box-border">
-      <p className="[font-family:'DM_Sans',sans-serif] text-base font-bold text-center text-[#ee393c] grow-0 shrink-0 basis-auto m-0 p-0">How it work?</p>
+      <p className="[font-family:'DM_Sans',sans-serif] text-base font-bold text-center text-[#ee393c] grow-0 shrink-0 basis-auto m-0 p-0">How does it work?</p>
       <p className="section-heading max-w-[700px] mx-auto mt-2">
         How PocketClass Works
       </p>
