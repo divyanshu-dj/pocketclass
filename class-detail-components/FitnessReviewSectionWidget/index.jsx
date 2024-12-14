97be3a0052d4f24b5e89f990897c627702bf320d
hafiz-mujtaba-ayyaz <majtaba.ayyaz@dubizzlelabs.com>
2024-12-15 02:20:36 +0500

    map moved upward

--- class-detail-components/FitnessReviewSectionWidget/index.jsx
+++ class-detail-components/FitnessReviewSectionWidget/index.jsx
@@ -1,7 +1,14 @@
+import DynamicButtonSection from "../DynamicButtonSection";
 import FitnessReviewSectionContainer from "../FitnessReviewSectionContainer";
 import ReviewSectionWidget from "../ReviewSectionWidget";
 
-function FitnessReviewSectionWidget({ classId, reviewCountsArray1, classTitle }) {
+function FitnessReviewSectionWidget({
+  classId,
+  reviewCountsArray1,
+  classTitle,
+  classData,
+  classCreatorData,
+}) {
   return (
     <div className="w-full lg:w-[88.00%] box-border mt-12">
       <FitnessReviewSectionContainer
@@ -9,6 +16,17 @@ function FitnessReviewSectionWidget({ classId, reviewCountsArray1, classTitle })
         reviewCountsArray1={reviewCountsArray1}
         classTitle={classTitle}
       />
+      <div
+        className="w-[90%] xl:hidden shadow-[1px_1px_7px_rgba(0,0,0,0.20)] bg-[white] box-border flex justify-start items-stretch flex-col  lg:max-w-[90%] pt-6 px-4 md:px-6 rounded-xl md:rounded-2xl lg:rounded-3x"
+        style={{ margin: "auto", marginTop: "20px" }}
+      >
+        <DynamicButtonSection
+          classId={classId}
+          classData={classData}
+          instructorId={classCreatorData?.userUid}
+          below={true}
+        />
+      </div>
       <ReviewSectionWidget classId={classId} />
     </div>
   );
