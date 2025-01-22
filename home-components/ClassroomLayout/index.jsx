0fa7aa21aebaca0664368796717d5fcb49708a47
Garv Goel <garvgoel2927@gmail.com>
2025-01-22 15:40:38 +0530

    Add Partners Carousel

--- home-components/ClassroomLayout/index.jsx
+++ home-components/ClassroomLayout/index.jsx
@@ -2,12 +2,14 @@ import ClassOverviewLayout from "../ClassOverviewLayout";
 import InstructorFinderView from "../InstructorFinderView";
 import ExtracurricularCard from "../ExtracurricularCard";
 import TeachingPassionBanner from "../TeachingPassionBanner";
+import CarousselSlider from "../../components/CarousselSlider";
 
 function ClassroomLayout() {
   return (
     <div className="flex justify-start items-stretch flex-col gap-[120px] md:gap-[100px]">
       <ClassOverviewLayout />
       <InstructorFinderView />
+      <CarousselSlider/>
       <ExtracurricularCard />
       <TeachingPassionBanner />
     </div>
