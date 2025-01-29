cea94c3eae0fdddbb6cdeb04a36e9124aefc5a0d
Andrew Liu <aliu5454@gmail.com>
2025-01-29 12:57:11 -0800

    Update index.jsx

--- home-components/ProgressTrackerWidget/index.jsx
+++ home-components/ProgressTrackerWidget/index.jsx
@@ -4,7 +4,7 @@ function ProgressTrackerWidget() {
       <p className="[font-family:'DM_Sans',sans-serif] text-base font-bold text-[#261f22] m-0 p-0">Step 3</p>
       <p className="[font-family:'DM_Sans',sans-serif] text-2xl font-bold text-[#261f22] mt-1 m-0 p-0">Book and Track Progress</p>
       <p className="[font-family:'DM_Sans',sans-serif] text-base font-medium leading-6 text-[#261f22] mt-4 m-0 p-0">
-        Seamlessly schedule classes, pay securely, and track your child&apos;s progress—all from one place.
+        Seamlessly schedule classes, pay securely, and track your progress—all from one place.
       </p>
     </div>
   );
