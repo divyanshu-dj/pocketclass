b3daad4d9a0a32d72c100ffe66447a462210da48
Andrew Liu <aliu5454@gmail.com>
2025-01-29 12:56:37 -0800

    Update index.jsx

--- home-components/ActivityDiscoverySection/index.jsx
+++ home-components/ActivityDiscoverySection/index.jsx
@@ -4,7 +4,7 @@ function ActivityDiscoverySection() {
       <p className="[font-family:'DM_Sans',sans-serif] text-base font-bold text-[#261f22] m-0 p-0">Step 1</p>
       <p className="[font-family:'DM_Sans',sans-serif] text-2xl font-bold text-[#261f22] mt-1 m-0 p-0">Discover Activities</p>
       <p className="[font-family:'DM_Sans',sans-serif] text-base font-medium leading-6 text-[#261f22] mt-4 m-0 p-0">
-        Search by activity, location, or age group to find the perfect fit for your child.
+        Search by activity category and review instructor ratings and descriptions to find the perfect fit for you.
       </p>
     </div>
   );
