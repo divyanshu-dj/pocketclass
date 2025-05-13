35171cdf596e11d1efac718fa0c1022b58c5d155
DEvARyan77 <devaryan7766@gmail.com>
2025-05-13 22:38:19 +0530

    Testing

--- class-detail-components/BookingComponent/index.jsx
+++ class-detail-components/BookingComponent/index.jsx
@@ -1506,36 +1506,37 @@ END:VCALENDAR`.trim();
 
 const CheckoutSkeleton = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center min-h-screen  bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded shadow-lg w-96 max-h-[80vh] overflow-y-auto animate-pulse">
-        {/* Go Back Button Skeleton */}
-        <div className="flex flex-row justify-end text-gray-300 mb-2">
-          <div className="h-4 w-16 bg-gray-300 rounded"></div>
-        </div>
+    <div></div>
+    // <div className="fixed inset-0 z-50 flex items-center justify-center min-h-screen  bg-black bg-opacity-50">
+    //   <div className="bg-white p-8 rounded shadow-lg w-96 max-h-[80vh] overflow-y-auto animate-pulse">
+    //     {/* Go Back Button Skeleton */}
+    //     <div className="flex flex-row justify-end text-gray-300 mb-2">
+    //       <div className="h-4 w-16 bg-gray-300 rounded"></div>
+    //     </div>
 
-        {/* Header Section Skeleton */}
-        <div className="flex flex-row items-center justify-between mb-4">
-          <div className="h-6 w-40 bg-gray-300 rounded"></div>
+    //     {/* Header Section Skeleton */}
+    //     <div className="flex flex-row items-center justify-between mb-4">
+    //       <div className="h-6 w-40 bg-gray-300 rounded"></div>
 
-          <div className="flex items-center">
-            <div className="h-4 w-20 bg-gray-300 rounded mr-2"></div>
-            <div className="h-4 w-12 bg-gray-300 rounded"></div>
-          </div>
-        </div>
+    //       <div className="flex items-center">
+    //         <div className="h-4 w-20 bg-gray-300 rounded mr-2"></div>
+    //         <div className="h-4 w-12 bg-gray-300 rounded"></div>
+    //       </div>
+    //     </div>
 
-        {/* Address Element Skeleton */}
-        <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
+    //     {/* Address Element Skeleton */}
+    //     <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
 
-        {/* Payment Element Skeleton */}
-        <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
-        <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
+    //     {/* Payment Element Skeleton */}
+    //     <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
+    //     <div className="h-14 w-full bg-gray-300 rounded mb-4"></div>
 
-        {/* Pay Button Skeleton */}
-        <div className="mt-4 p-2 bg-gray-400 text-white rounded w-full text-center">
-          Processing...
-        </div>
-      </div>
-    </div>
+    //     {/* Pay Button Skeleton */}
+    //     <div className="mt-4 p-2 bg-gray-400 text-white rounded w-full text-center">
+    //       Processing...
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
