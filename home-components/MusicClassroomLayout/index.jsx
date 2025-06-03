57c06987a8c89b3f24084292741e076a27fcd6f7
Andrew Liu <aliu5454@gmail.com>
2025-06-03 05:16:35 -0700

    Revert "New filter added"

--- home-components/MusicClassroomLayout/index.jsx
+++ home-components/MusicClassroomLayout/index.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import MusicSelector from "../MusicSelector";
 import ClassroomLayout1 from "../ClassroomLayout1";
 import TopClassesSection from "../TopInstructorsSection";
 
-function MusicClassroomLayout({ activeCategory }) {
+function MusicClassroomLayout() {
+  const [activeFilter, setActiveFilter] = useState(null);
+
   return (
     <div className="mt-[-3px] flex justify-start items-stretch flex-col w-[100.00%] box-border">
-      <MusicSelector selectedCategory={activeCategory} />
-      <ClassroomLayout1 />
+      <MusicSelector onFilterChange={setActiveFilter} />
+      <ClassroomLayout1 activeFilter={activeFilter} />
     </div>
   );
 }
