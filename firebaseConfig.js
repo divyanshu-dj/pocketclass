873f2f8158dd47f1c0fe111cf908505230697404
DEvARyan77 <devaryan7766@gmail.com>
2025-06-12 01:05:34 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth"
 
 // Production
 const firebaseConfig = {
-    apiKey: "AIzaSyBKP8mEj1UHijTvkZ00TWqkRdIsKF3qpzk",
-    authDomain: "pocket-class-bf921.firebaseapp.com",
-    projectId: "pocket-class-bf921",
-    storageBucket: "pocket-class-bf921.appspot.com",
-    messagingSenderId: "270140022594",
-    appId: "1:270140022594:web:d25d81f3a3b7070df332db"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 // Development
 // const firebaseConfig = {
