3c398a8eab2f9f263ec554d8e39bdde51f4938ae
Garv Goel <garvgoel@Mac.lan>
2025-07-26 10:28:31 +0530

    Deployment fixes

--- components/clientDetails.js
+++ components/clientDetails.js
@@ -29,18 +29,8 @@ import "react-quill/dist/quill.snow.css";
 const quillModules = {
   toolbar: [
     ["bold", "italic", "underline", "strike"],
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ list: "ordered" }, { list: "bullet" }],
-    [{ script: "sub" }, { script: "super" }],
-    [{ indent: "-1" }, { indent: "+1" }],
-    [{ direction: "rtl" }],
-    [{ size: ["small", false, "large", "huge"] }],
-    [{ color: [] }, { background: [] }],
-    [{ font: [] }],
-    [{ align: [] }],
     ["link", "image", "video"],
-    ["blockquote", "code-block"],
-    ["clean"],
   ],
   clipboard: {
     // toggle to add extra line breaks when pasting HTML:
